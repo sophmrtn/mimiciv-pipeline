c333b6600e69d9531501ea2bfaf7390c710349cf
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-04-12 10:50:51 +0100

    Temporarily ignore references to Bert model due to installation issue

--- model/behrt_model.py
+++ model/behrt_model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import pytorch_pretrained_bert as Bert
+# import pytorch_pretrained_bert as Bert
 from torch.utils.data.dataset import Dataset
 import numpy as np
 import os
@@ -30,7 +30,7 @@ class BertEmbeddings(nn.Module):
         self.posi_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size). \
             from_pretrained(embeddings=self._init_posi_embedding(config.max_position_embeddings, config.hidden_size))
 
-        self.LayerNorm = Bert.modeling.BertLayerNorm(config.hidden_size, eps=1e-12)
+        # self.LayerNorm = Bert.modeling.BertLayerNorm(config.hidden_size, eps=1e-12)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, word_ids, age_ids=None, gender_ids=None, ethni_ids=None, ins_ids=None, seg_ids=None,
@@ -81,95 +81,95 @@ class BertEmbeddings(nn.Module):
         return torch.tensor(lookup_table)
 
 
-class BertModel(Bert.modeling.BertPreTrainedModel):
-    def __init__(self, config):
-        super(BertModel, self).__init__(config)
-        self.embeddings = BertEmbeddings(config=config)
-        self.encoder = Bert.modeling.BertEncoder(config=config)
-        self.pooler = Bert.modeling.BertPooler(config)
-        self.apply(self.init_bert_weights)
+# class BertModel(Bert.modeling.BertPreTrainedModel):
+#     def __init__(self, config):
+#         super(BertModel, self).__init__(config)
+#         self.embeddings = BertEmbeddings(config=config)
+#         self.encoder = Bert.modeling.BertEncoder(config=config)
+#         self.pooler = Bert.modeling.BertPooler(config)
+#         self.apply(self.init_bert_weights)
 
-    def forward(self, input_ids, age_ids=None, gender_ids=None, ethni_ids=None, ins_ids=None, seg_ids=None,
-                posi_ids=None, attention_mask=None,
-                output_all_encoded_layers=True):
-        if attention_mask is None:
-            attention_mask = torch.ones_like(input_ids)
-        if age_ids is None:
-            age_ids = torch.zeros_like(input_ids)
-        if gender_ids is None:
-            gender_ids = torch.zeros_like(input_ids)
-        if ethni_ids is None:
-            ethni_ids = torch.zeros_like(input_ids)
-        if ins_ids is None:
-            ins_ids = torch.zeros_like(input_ids)
-        if seg_ids is None:
-            seg_ids = torch.zeros_like(input_ids)
-        if posi_ids is None:
-            posi_ids = torch.zeros_like(input_ids)
+#     def forward(self, input_ids, age_ids=None, gender_ids=None, ethni_ids=None, ins_ids=None, seg_ids=None,
+#                 posi_ids=None, attention_mask=None,
+#                 output_all_encoded_layers=True):
+#         if attention_mask is None:
+#             attention_mask = torch.ones_like(input_ids)
+#         if age_ids is None:
+#             age_ids = torch.zeros_like(input_ids)
+#         if gender_ids is None:
+#             gender_ids = torch.zeros_like(input_ids)
+#         if ethni_ids is None:
+#             ethni_ids = torch.zeros_like(input_ids)
+#         if ins_ids is None:
+#             ins_ids = torch.zeros_like(input_ids)
+#         if seg_ids is None:
+#             seg_ids = torch.zeros_like(input_ids)
+#         if posi_ids is None:
+#             posi_ids = torch.zeros_like(input_ids)
 
-        # We create a 3D attention mask from a 2D tensor mask.
-        # Sizes are [batch_size, 1, 1, to_seq_length]
-        # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
-        # this attention mask is more simple than the triangular masking of causal attention
-        # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
-        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+#         # We create a 3D attention mask from a 2D tensor mask.
+#         # Sizes are [batch_size, 1, 1, to_seq_length]
+#         # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
+#         # this attention mask is more simple than the triangular masking of causal attention
+#         # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
+#         extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
 
-        # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
-        # masked positions, this operation will create a tensor which is 0.0 for
-        # positions we want to attend and -10000.0 for masked positions.
-        # Since we are adding it to the raw scores before the softmax, this is
-        # effectively the same as removing these entirely.
-        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
-        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+#         # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+#         # masked positions, this operation will create a tensor which is 0.0 for
+#         # positions we want to attend and -10000.0 for masked positions.
+#         # Since we are adding it to the raw scores before the softmax, this is
+#         # effectively the same as removing these entirely.
+#         extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+#         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
-        embedding_output = self.embeddings(input_ids, age_ids, gender_ids, ethni_ids, ins_ids, seg_ids, posi_ids)
-        encoded_layers = self.encoder(embedding_output,
-                                      extended_attention_mask,
-                                      output_all_encoded_layers=output_all_encoded_layers)
-        sequence_output = encoded_layers[-1]
-        pooled_output = self.pooler(sequence_output)
-        if not output_all_encoded_layers:
-            encoded_layers = encoded_layers[-1]
-        return encoded_layers, pooled_output
+#         embedding_output = self.embeddings(input_ids, age_ids, gender_ids, ethni_ids, ins_ids, seg_ids, posi_ids)
+#         encoded_layers = self.encoder(embedding_output,
+#                                       extended_attention_mask,
+#                                       output_all_encoded_layers=output_all_encoded_layers)
+#         sequence_output = encoded_layers[-1]
+#         pooled_output = self.pooler(sequence_output)
+#         if not output_all_encoded_layers:
+#             encoded_layers = encoded_layers[-1]
+#         return encoded_layers, pooled_output
 
 
-class BertForEHRPrediction(Bert.modeling.BertPreTrainedModel):
-    def __init__(self, config, num_labels):
-        super(BertForEHRPrediction, self).__init__(config)
-        self.num_labels = num_labels
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, num_labels)
-        self.apply(self.init_bert_weights)
+# class BertForEHRPrediction(Bert.modeling.BertPreTrainedModel):
+#     def __init__(self, config, num_labels):
+#         super(BertForEHRPrediction, self).__init__(config)
+#         self.num_labels = num_labels
+#         self.bert = BertModel(config)
+#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+#         self.classifier = nn.Linear(config.hidden_size, num_labels)
+#         self.apply(self.init_bert_weights)
 
-    def forward(self, input_ids, age_ids=None, gender_ids=None, ethni_ids=None, ins_ids=None, seg_ids=None, posi_ids=None, attention_mask=None):
-        _, pooled_output = self.bert(input_ids, age_ids, gender_ids, ethni_ids, ins_ids, seg_ids, posi_ids, attention_mask,
-                                     output_all_encoded_layers=False)
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
+#     def forward(self, input_ids, age_ids=None, gender_ids=None, ethni_ids=None, ins_ids=None, seg_ids=None, posi_ids=None, attention_mask=None):
+#         _, pooled_output = self.bert(input_ids, age_ids, gender_ids, ethni_ids, ins_ids, seg_ids, posi_ids, attention_mask,
+#                                      output_all_encoded_layers=False)
+#         pooled_output = self.dropout(pooled_output)
+#         logits = self.classifier(pooled_output)
 
-        return logits
+#         return logits
 
-class BertConfig(Bert.modeling.BertConfig):
-    def __init__(self, config):
-        super(BertConfig, self).__init__(
-            vocab_size_or_config_json_file=config.get('vocab_size'),
-            hidden_size=config['hidden_size'],
-            num_hidden_layers=config.get('num_hidden_layers'),
-            num_attention_heads=config.get('num_attention_heads'),
-            intermediate_size=config.get('intermediate_size'),
-            hidden_act=config.get('hidden_act'),
-            hidden_dropout_prob=config.get('hidden_dropout_prob'),
-            attention_probs_dropout_prob=config.get('attention_probs_dropout_prob'),
-            max_position_embeddings = config.get('max_position_embedding'),
-            initializer_range=config.get('initializer_range'),
-        )
-        self.seg_vocab_size = config.get('seg_vocab_size')
-        self.age_vocab_size = config.get('age_vocab_size')
-        self.gender_vocab_size = config.get('gender_vocab_size')
-        self.ethni_vocab_size = config.get('ethni_vocab_size')
-        self.ins_vocab_size = config.get('ins_vocab_size')
-        self.number_output = config.get('number_output')
+# class BertConfig(Bert.modeling.BertConfig):
+#     def __init__(self, config):
+#         super(BertConfig, self).__init__(
+#             vocab_size_or_config_json_file=config.get('vocab_size'),
+#             hidden_size=config['hidden_size'],
+#             num_hidden_layers=config.get('num_hidden_layers'),
+#             num_attention_heads=config.get('num_attention_heads'),
+#             intermediate_size=config.get('intermediate_size'),
+#             hidden_act=config.get('hidden_act'),
+#             hidden_dropout_prob=config.get('hidden_dropout_prob'),
+#             attention_probs_dropout_prob=config.get('attention_probs_dropout_prob'),
+#             max_position_embeddings = config.get('max_position_embedding'),
+#             initializer_range=config.get('initializer_range'),
+#         )
+#         self.seg_vocab_size = config.get('seg_vocab_size')
+#         self.age_vocab_size = config.get('age_vocab_size')
+#         self.gender_vocab_size = config.get('gender_vocab_size')
+#         self.ethni_vocab_size = config.get('ethni_vocab_size')
+#         self.ins_vocab_size = config.get('ins_vocab_size')
+#         self.number_output = config.get('number_output')
 
 class TrainConfig(object):
     def __init__(self, config):
